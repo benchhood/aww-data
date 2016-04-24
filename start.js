144dfc3f758c47eb5a7746ee45ec7788fd6e4464
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-04-24 22:49:01 +0530

    printed env

--- start.js
+++ start.js
@@ -2,6 +2,10 @@ var setup = require('./setup.js');
 var main = require('./main.js');
 var isSetupSuccess = false;
 
+console.log('SHIPPABLE_JOB_NUMBER' + process.env.SHIPPABLE_JOB_NUMBER);
+
+console.log(JSON.stringify(process.env));
+
 // setup environment variables for local environment
 if (!process.env.SHIPPABLE_JOB_NUMBER) {
   isSetupSuccess = setup();
