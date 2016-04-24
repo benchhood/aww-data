ecdf3e5353e2bd25568635209dc00a12bfe75e1e
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-04-24 23:02:15 +0530

    made setup to be called conditionally

--- printenv.js
+++
@@ -1,5 +0,0 @@
-console.log('inside printenv');
-console.log(JSON.stringify(process.env));
-console.log('SHIPPABLE : ' + process.env.SHIPPABLE);
-console.log(!!process.env.SHIPPABLE);
-console.log(!process.env.SHIPPABLE);
--- start.js
+++ start.js
@@ -1,13 +1,9 @@
-var setup = require('./setup.js');
 var main = require('./main.js');
 var isSetupSuccess = false;
 
-console.log('SHIPPABLE_JOB_NUMBER' + process.env.SHIPPABLE_JOB_NUMBER);
-
-console.log(JSON.stringify(process.env));
-
 // setup environment variables for local environment
 if (!process.env.SHIPPABLE) {
+  var setup = require('./setup.js');
   isSetupSuccess = setup();
 }
 else {
